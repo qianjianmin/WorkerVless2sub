9fc8fb9f1ce78392edd22de014587d4ae6a2b63d
qianjianmin <127366530+qianjianmin@users.noreply.github.com>
2024-06-25 13:34:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -40,7 +40,8 @@ let link = '';
 let edgetunnel = 'ed';
 let RproxyIP = 'false';
 let proxyIPs = [//无法匹配到节点名就随机分配以下ProxyIP域名
-	'proxyip.aliyun.fxxk.dedyn.io',
+	'proxyip.us.fxxk.dedyn.io',
+	//'proxyip.aliyun.fxxk.dedyn.io',
 	//'proxyip.multacom.fxxk.dedyn.io',
 	//'proxyip.vultr.fxxk.dedyn.io',
 ];
