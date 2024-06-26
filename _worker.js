f150200193f8ac1a4a6ac59048e72dc185328b4f
qianjianmin <127366530+qianjianmin@users.noreply.github.com>
2024-06-26 13:08:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -27,7 +27,7 @@ let addressesnotlsapi = [
 	//'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
 
-let DLS = 8192;//速度下限
+let DLS = 1024;//速度下限
 let addressescsv = [
 	'https://raw.githubusercontent.com/qianjianmin/WorkerVless2sub/main/ip.csv',
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
